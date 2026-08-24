f2b11c1b771c570cb6b4a82adc5e2e24d0f1071c
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-08-24 20:54:51 +0530

    Add C++ solution for Increasing_Array

---
+++ Introductory_Problems/Increasing_Array/Increasing_Array.cpp
@@ -0,0 +1,25 @@
+/*
+ * Problem Name: Increasing_Array
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-24
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin >> n;
+    vector<long long>a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    long long moves = 0;
+    for(int i = 1; i < n; i++){
+        if(a[i] < a[i - 1]){
+            moves += (a[i - 1] - a[i]);
+        }
+    }
+    cout << moves << "\n";
+}
