de3e2756a9207349b79c620ea1ca3e52cf84f562
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-08-25 22:36:49 +0530

    Add C++ solution for Building_Roads

---
+++ Graph_Algorithms/Building_Roads/Building_Roads.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Building_Roads
+ * Language: C++
+ * Category: Graph_Algorithms
+ * Date: 2026-08-25
+ */
+
+input/code.cpp: In function 'int main()':
+input/code.cpp:70:23: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   70 |     for (int i = 1; i < components.size(); i++) {
+      |                     ~~^~~~~~~~~~~~~~~~~~~
