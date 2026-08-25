a2cda1606890731f2b98a6179030d9486620f895
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-08-25 23:36:22 +0530

    Add C++ solution for Round_Trip

---
+++ Graph_Algorithms/Round_Trip/Round_Trip.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Round_Trip
+ * Language: C++
+ * Category: Graph_Algorithms
+ * Date: 2026-08-25
+ */
+
+input/code.cpp: In function 'int main()':
+input/code.cpp:39:34: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   39 |                 for(int i = 0; i < path.size(); i++){
+      |                                ~~^~~~~~~~~~~~~
