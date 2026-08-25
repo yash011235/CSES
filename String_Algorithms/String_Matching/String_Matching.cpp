be5615415cdb74fdadf64eb38666ec04b1378831
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-08-25 14:46:21 +0530

    Add C++ solution for String_Matching

--- String_Algorithms/String_Matching/String_Matching.cpp
+++ String_Algorithms/String_Matching/String_Matching.cpp
@@ -5,25 +5,7 @@
  * Date: 2026-08-25
  */
 
-#include<bits/stdc++.h>
-using namespace std;
- 
-int main(){
-    string t, p;
-    int cnt = 0;
-    cin >> t >> p;
-    int n = t.size();
-    int m = p.size();
-    for(int i = 0; i <= (n - m); i++){
-        bool flag = true;
-        for(int j = 0; j < m; j++){
-            if(t[i + j] != p[j]){
-                flag = false;
-            }
-        }
-        if(flag){
-            cnt++;
-        }
-    }
-    cout << cnt << "\n";
-}
+input/code.cpp: In function 'void constructLPS(std::string&, std::vector<int>&)':
+input/code.cpp:8:13: warning: comparison of integer expressions of different signedness: 'int' and 'std::__cxx11::basic_string<char>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+    8 |     while(i < p.size()){
+      |           ~~^~~~~~~~~~
