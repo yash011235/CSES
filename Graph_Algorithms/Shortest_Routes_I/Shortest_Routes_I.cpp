3e0b1702d71932be5ff5af6aad7f6022aed95bf9
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-08-26 00:04:00 +0530

    Add C++ solution for Shortest_Routes_I

---
+++ Graph_Algorithms/Shortest_Routes_I/Shortest_Routes_I.cpp
@@ -0,0 +1,44 @@
+/*
+ * Problem Name: Shortest_Routes_I
+ * Language: C++
+ * Category: Graph_Algorithms
+ * Date: 2026-08-25
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<vector<pair<int, int>>>adj(n);
+    for(int i = 0; i < m; i++){
+        int u, v, w;
+        cin >> u >> v >> w;
+        u--;
+        v--;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+    vector<int>dist(n, INT_MAX);
+    dist[0] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+    pq.push({0, 0});
+    while(!pq.empty()){
+        int d = pq.top().first;
+        int node = pq.top().second;
+        pq.pop();
+        if(d > dist[node]){
+            continue;
+        }
+        for(auto &[nei, w] : adj[node]){
+            if(dist[nei] > dist[node] + w){
+                dist[nei] = w + dist[node];
+                pq.push({dist[nei], nei});
+            }
+        }
+    }
+    for(int i = 0; i < n; i++){
+        cout << dist[i] << " ";
+    }
+    cout << "\n";
+}
