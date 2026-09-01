f968eb167577eda17be8e6384f37f36d2fbced77
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-09-01 17:12:50 +0530

    Add C++ solution for Book_Shop

--- Dynamic_Programming/Book_Shop/Book_Shop.cpp
+++ Dynamic_Programming/Book_Shop/Book_Shop.cpp
@@ -5,36 +5,29 @@
  * Date: 2026-09-01
  */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int n;
-int x;
-vector<vector<int>>dp;
-int solve(vector<int>&pages, vector<int>&prices, int i, int amount){
-    if(amount < 0 || i >= n){
-        return 0;
-    }
-    if(dp[i][amount] != -1){
-        return dp[i][amount];
-    }
-    int incl = 0;
-    if(amount >= prices[i])
-    incl = pages[i] + solve(pages, prices, i + 1, amount - prices[i]);
-    int excl = 0 + solve(pages, prices, i + 1, amount);
-    return dp[i][amount] = max(incl, excl);
-}
  
-int main(){
+int main() {
+    int n, x;
     cin >> n >> x;
-    vector<int>prices(n);
-    vector<int>pages(n);
-    dp.assign(n, vector<int>(x + 1, -1));
-    for(int i = 0; i < n; i++){
-        cin >> prices[i];
-    }
-    for(int i = 0; i < n; i++){
+ 
+    vector<int> price(n), pages(n);
+ 
+    for (int i = 0; i < n; i++)
+        cin >> price[i];
+ 
+    for (int i = 0; i < n; i++)
         cin >> pages[i];
+ 
+    vector<int> dp(x + 1, 0);
+ 
+    for (int i = 0; i < n; i++) {
+        for (int a = x; a >= price[i]; a--) {
+            dp[a] = max(dp[a],
+                        pages[i] + dp[a - price[i]]);
+        }
     }
-    int ans = solve(pages, prices, 0, x);
-    cout << ans << "\n";
+ 
+    cout << dp[x] << '\n';
 }
