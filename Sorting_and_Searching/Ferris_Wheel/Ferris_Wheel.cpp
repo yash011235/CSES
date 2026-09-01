8252df2df9a289a691ab5bc16ab1bc181399a13d
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-09-01 22:14:24 +0530

    Add C++ solution for Ferris_Wheel

---
+++ Sorting_and_Searching/Ferris_Wheel/Ferris_Wheel.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Ferris_Wheel
+ * Language: C++
+ * Category: Sorting_and_Searching
+ * Date: 2026-09-01
+ */
+
+input/code.cpp: In function 'bool solve(std::vector<long long int>&, long long int, int)':
+input/code.cpp:7:22: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<long long int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+    7 |     for(int i = 0; i < a.size(); i++){
+      |                    ~~^~~~~~~~~~
