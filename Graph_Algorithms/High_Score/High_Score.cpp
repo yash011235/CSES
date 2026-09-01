0029149f2c6d8e4bd540a894e8d5c414be9cc378
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-09-01 15:18:10 +0530

    Add C++ solution for High_Score

--- Graph_Algorithms/High_Score/High_Score.cpp
+++ Graph_Algorithms/High_Score/High_Score.cpp
@@ -5,7 +5,53 @@
  * Date: 2026-09-01
  */
 
-input/code.cpp: In function 'int main()':
-input/code.cpp:20:19: warning: unused variable 'd' [-Wunused-variable]
-   20 |         long long d = q.front().first;
-      |                   ^
+#include <bits/stdc++.h>
+using namespace std;
+ 
+struct Edge {
+    int u, v;
+    long long w;
+};
+ 
+int main() {
+    int n, m;
+    cin >> n >> m;
+ 
+    vector<Edge> edges;
+ 
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        long long w;
+ 
+        cin >> u >> v >> w;
+ 
+        u--;
+        v--;
+ 
+        edges.push_back({u, v, w});
+    }
+ 
+    vector<long long> dist(n, LLONG_MIN);
+    dist[0] = 0;
+ 
+    for (int i = 0; i < n - 1; i++) {
+        for (auto &e : edges) {
+            if (dist[e.u] != LLONG_MIN &&
+                dist[e.v] < dist[e.u] + e.w) {
+ 
+                dist[e.v] = dist[e.u] + e.w;
+            }
+        }
+    }
+ 
+    for (auto &e : edges) {
+        if (dist[e.u] != LLONG_MIN &&
+            dist[e.v] < dist[e.u] + e.w) {
+ 
+            cout << -1 << '\n';
+            return 0;
+        }
+    }
+ 
+    cout << dist[n - 1] << '\n';
+}
