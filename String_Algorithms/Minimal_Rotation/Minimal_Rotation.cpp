7a9596ff60c14ef7a68ca260ad1e83a8a43249e7
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-09-01 16:37:01 +0530

    Add C++ solution for Minimal_Rotation

--- String_Algorithms/Minimal_Rotation/Minimal_Rotation.cpp
+++ String_Algorithms/Minimal_Rotation/Minimal_Rotation.cpp
@@ -11,14 +11,33 @@ using namespace std;
 int main() {
     string s;
     cin >> s;
- 
+    string t = s + s;
+    int i = 0;
+    int j = 1;
+    int k = 0;
     int n = s.length();
-    string ans = s;
- 
-    for (int i = 0; i < n - 1; i++) {
-        rotate(s.begin(), s.end() - 1, s.end());
-        ans = min(ans, s);
+    while(i < n && j < n && k < n){
+        if(t[i + k] == t[j + k]){
+            k++;
+        }
+        else if(t[i + k] > t[j + k]){
+            i = i + k + 1;
+            if(i == j){
+                i++;
+            }
+            k = 0;
+        }
+        else{
+            j = j + k + 1;
+            if(i == j){
+                j++;
+            }
+            k = 0;
+        }
     }
- 
+    
+    int start = min(i, j);
+    
+    string ans = t.substr(start, n);
     cout << ans << "\n";
 }
