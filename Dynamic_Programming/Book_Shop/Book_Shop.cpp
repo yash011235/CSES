f13bf225c18e7528fa4111e4213af903518d4115
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-09-01 17:04:45 +0530

    Add C++ solution for Book_Shop

--- Dynamic_Programming/Book_Shop/Book_Shop.cpp
+++ Dynamic_Programming/Book_Shop/Book_Shop.cpp
@@ -9,21 +9,26 @@
 using namespace std;
 int n;
 int x;
+vector<vector<int>>dp;
 int solve(vector<int>&pages, vector<int>&prices, int i, int amount){
     if(amount < 0 || i >= n){
         return 0;
+    }
+    if(dp[i][amount] != -1){
+        return dp[i][amount];
     }
     int incl = 0;
     if(amount >= prices[i])
     incl = pages[i] + solve(pages, prices, i + 1, amount - prices[i]);
     int excl = 0 + solve(pages, prices, i + 1, amount);
-    return max(incl, excl);
+    return dp[i][amount] = max(incl, excl);
 }
  
 int main(){
     cin >> n >> x;
     vector<int>prices(n);
     vector<int>pages(n);
+    dp.assign(n, vector<int>(x + 1, -1));
     for(int i = 0; i < n; i++){
         cin >> prices[i];
     }
