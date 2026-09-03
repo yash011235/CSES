973d0cdf7649ffda1b4b1bf3f04f5f9b86b4c155
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-09-03 22:35:00 +0530

    Add C++ solution for Grid_Paths_I

--- Dynamic_Programming/Grid_Paths_I/Grid_Paths_I.cpp
+++ Dynamic_Programming/Grid_Paths_I/Grid_Paths_I.cpp
@@ -5,32 +5,43 @@
  * Date: 2026-09-03
  */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+ 
 int n;
-vector<vector<int>>dp;
-int solve(int i, int j, vector<vector<char>>&grid){
-    if(i >= n || j >= n || grid[i][j] == '*'){
+vector<vector<long long>> dp;
+const int MOD = 1e9 + 7;
+long long solve(int i, int j, vector<vector<char>>& grid) {
+    if(i >= n || j >= n || grid[i][j] == '*') {
         return 0;
     }
-    if(i == (n - 1) && j == (n - 1)){
+ 
+    if(i == n - 1 && j == n - 1) {
         return 1;
     }
-    if(dp[i][j] != -1){
+ 
+    if(dp[i][j] != -1) {
         return dp[i][j];
     }
-    return dp[i][j] = solve(i + 1, j, grid) + solve(i, j + 1, grid);
+ 
+    long long down = solve(i + 1, j, grid) % MOD;
+    long long right = solve(i, j + 1, grid) % MOD;
+ 
+    return dp[i][j] = (down + right) % MOD;
 }
  
-int main(){
+int main() {
     cin >> n;
-    vector<vector<char>>grid(n, vector<char>(n));
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+ 
+    vector<vector<char>> grid(n, vector<char>(n));
+ 
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
             cin >> grid[i][j];
         }
     }
-    dp.assign(n, vector<int>(n, -1));
-    int ans = solve(0, 0, grid);
-    cout << ans << "\n";
+ 
+    dp.assign(n, vector<long long>(n, -1));
+ 
+    cout << (solve(0, 0, grid) % MOD) << "\n";
 }
