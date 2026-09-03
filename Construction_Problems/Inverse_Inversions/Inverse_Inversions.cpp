8a56ab3d320b0c43cd8e315f9c401c559835f165
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-09-03 21:27:39 +0530

    Add C++ solution for Inverse_Inversions

--- Construction_Problems/Inverse_Inversions/Inverse_Inversions.cpp
+++ Construction_Problems/Inverse_Inversions/Inverse_Inversions.cpp
@@ -5,63 +5,72 @@
  * Date: 2026-09-03
  */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
  
-void rotate(vector<long long>&a, long long num) {
-	long long curr = num + 1;
-	long long n = a.size();
-	for(long long i = 0; i < (n - num); i++) {
-		a[i] = curr;
-		curr++;
-	}
-	a[n - num] = num;
-	for(long long i = n - num + 1; i < n; i++) {
-		a[i] = a[i - 1] - 1;
-	}
+void rotate(vector<long long>& a, long long num) {
+    long long curr = num + 1;
+    long long n = a.size();
+ 
+    for(long long i = 0; i < n - num; i++) {
+        a[i] = curr;
+        curr++;
+    }
+ 
+    a[n - num] = num;
+ 
+    for(long long i = n - num + 1; i < n; i++) {
+        a[i] = a[i - 1] - 1;
+    }
 }
  
-void rotate2(vector<long long>&a, long long rem, long long num) {
-	for(long long i = 0; i < rem; i++) {
-		a[i] = a[i + 1];
-	}
-	a[rem] = num;
+void rotate2(vector<long long>& a, long long rem, long long num) {
+    for(long long i = 0; i < rem; i++) {
+        a[i] = a[i + 1];
+    }
+ 
+    a[rem] = num;
 }
  
 int main() {
-	long long n, k;
-	cin >> n >> k;
-	vector<long long>a(n);
-	for(long long i = 0; i < n; i++) {
-		a[i] = (i + 1);
-	}
+    long long n, k;
+    cin >> n >> k;
  
-    if(k == ((n * (n - 1)) / 2)){
-        for(long long i = n; i >= 1; i--){
+    vector<long long> a(n);
+ 
+    for(long long i = 0; i < n; i++) {
+        a[i] = i + 1;
+    }
+ 
+    if(k == (n * (n - 1)) / 2) {
+        for(long long i = n; i >= 1; i--) {
             cout << i << " ";
         }
         cout << "\n";
         return 0;
     }
-	for(long long x = 1; x <= ((n - 1) * n) / 2; x++) {
-		if(x > k) {
-			break;
-		}
-		else if(x == k) {
-			long long div = (x + n - 2)/ (n - 1);
-			long long rem = x % (n - 1);
-            if(rem == 0){
-                rem = (n - 1);
-            }
-			for(long long num = 1; num < div; num++) {
-				rotate(a, num);
-			}
  
-			rotate2(a, rem, div);
-		}
-	}
-	for(long long &e : a) {
-		cout << e << " ";
-	}
+    long long x = k;
  
+    long long div = (x + n - 2) / (n - 1);
+ 
+    long long rem = x % (n - 1);
+ 
+    if(rem == 0) {
+        rem = n - 1;
+    }
+ 
+    for(long long num = 1; num < div; num++) {
+        rotate(a, num);
+    }
+ 
+    rotate2(a, rem, div);
+ 
+    for(long long &e : a) {
+        cout << e << " ";
+    }
+ 
+    cout << "\n";
+ 
+    return 0;
 }
