34286814daa435f20b368d1597c888f889b684e7
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-09-03 23:15:37 +0530

    Add C++ solution for Coin_Combinations_I

--- Dynamic_Programming/Coin_Combinations_I/Coin_Combinations_I.cpp
+++ Dynamic_Programming/Coin_Combinations_I/Coin_Combinations_I.cpp
@@ -10,34 +10,45 @@ using namespace std;
  
 int n;
 long long x;
+ 
 const int MOD = 1e9 + 7;
-vector<long long>dp;
-long long solve(vector<long long>&a, long long x){
-    if(x == 0){
+ 
+vector<long long> dp;
+ 
+long long solve(vector<long long>& a, long long x) {
+    if(x == 0) {
         return 1;
     }
-    if(x < 0){
-        return INT_MIN;
+ 
+    if(x < 0) {
+        return 0;
     }
-    if(dp[x] != -1){
+ 
+    if(dp[x] != -1) {
         return dp[x];
     }
-    int ans = 0;
-    for(int i = 0; i < n; i++){
-        int temp = solve(a, x - a[i]);
-        if(temp != INT_MIN){
-            ans = (ans % MOD + temp % MOD) % MOD;
-        }
+ 
+    long long ans = 0;
+ 
+    for(int i = 0; i < n; i++) {
+        long long temp = solve(a, x - a[i]);
+ 
+        ans = (ans + temp) % MOD;
     }
-    return dp[x] = ans % MOD;
+ 
+    return dp[x] = ans;
 }
  
 int main() {
     cin >> n >> x;
-    dp.assign(x + 1, -1);
-    vector<long long>a(n);
-    for(int i = 0; i < n; i++){
+ 
+    vector<long long> a(n);
+ 
+    for(int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    cout << solve(a, x) % MOD;
+ 
+    dp.assign(x + 1, -1);
+ 
+    cout << solve(a, x) << "\n";
 }
