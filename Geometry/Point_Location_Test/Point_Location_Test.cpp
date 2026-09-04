b0b4080f2ce5d263500d36a2ff6f8e0a56bd16f0
yash011235 <yashasvi.mahawar.cse24@itbhu.ac.in>
2026-09-04 20:14:39 +0530

    Add C++ solution for Point_Location_Test

---
+++ Geometry/Point_Location_Test/Point_Location_Test.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem Name: Point_Location_Test
+ * Language: C++
+ * Category: Geometry
+ * Date: 2026-09-04
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int tt;
+    cin >> tt;
+    while(tt--){
+        int x1, y1, x2, y2, x3, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    if(((x2 - x1) * (y3 - y1)) == ((x3 - x1) * (y2 - y1))){
+        cout << "TOUCH" << "\n";
+    }
+    else if(((x2 - x1) * (y3 - y1)) > ((x3 - x1) * (y2 - y1))){
+        cout << "LEFT" << "\n";
+    }
+    else{
+        cout << "RIGHT" << "\n";
+    }
+    }
+    
+}
